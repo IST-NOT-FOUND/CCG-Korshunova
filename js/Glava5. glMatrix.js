7556a922ab4536f0e9fca92e3b21977b70dad7d9
Anastasia <k96a49o@gmail.com>
2017-12-25 23:53:39 +0500

    Изменения в задании к 5 главе

--- js/Glava5. glMatrix.js
+++ js/Glava5. glMatrix.js
@@ -91,7 +91,7 @@ function draw() {
 function setupWebGL()
 {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT);
 
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     mat4.perspective(pMatrix, 1.04, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
