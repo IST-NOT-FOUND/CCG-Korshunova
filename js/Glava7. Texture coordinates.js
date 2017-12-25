edb1eb98233c04b5e655a19dc376663fde8bfbbe
Anastasia <k96a49o@gmail.com>
2017-12-25 23:55:15 +0500

    Изменения в задании к 7 главе

--- js/Glava7. Texture coordinates.js
+++ js/Glava7. Texture coordinates.js
@@ -113,7 +113,7 @@ function setTextures(){
         handleTextureLoaded(image, texture);
     }
 
-    image.src = "brickwall.png";
+    image.src = "../../image/wall.png";
 
     shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
     gl.uniform1i(shaderProgram.samplerUniform, 0);
