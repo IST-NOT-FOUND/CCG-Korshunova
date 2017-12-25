8ac108c0df6cf662bb881a96b81c2f1ffae38a26
Anastasia <k96a49o@gmail.com>
2017-12-25 23:53:22 +0500

    Изменения в задании к 4 главе

--- js/Glava4. Color setting.js
+++ js/Glava4. Color setting.js
@@ -85,23 +85,23 @@ function draw() {
 
 window.onload=function(){
 
-        var canvas = document.getElementById("canvas3D");
-        try {
-            gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-        }
-        catch(e) {}
-
-        if (!gl) {
-            alert("Ваш браузер не поддерживает WebGL");
-        }
-        if(gl){
-            gl.viewportWidth = canvas.width;
-            gl.viewportHeight = canvas.height;
-
-            initShaders();
-
-            initBuffers();
-
-            draw();
-        }
+    var canvas = document.getElementById("canvas3D");
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
     }
+    catch(e) {}
+
+    if (!gl) {
+        alert("Ваш браузер не поддерживает WebGL");
+    }
+    if(gl){
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+
+        initShaders();
+
+        initBuffers();
+
+        draw();
+    }
+}
